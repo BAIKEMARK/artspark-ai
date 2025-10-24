15a4647826a3385fa79e2e784520661bf9037902
BAIKEMARK <zdl510510@126.com>
2025-10-24 12:39:35 +0800

    feat(env): 添加 .env.example fix(frontend): 更换 phosphor-icons 图标集为 bold 样式，解决图标加载不出来的BUG

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import '@phosphor-icons/web/regular' // 使用 regular 图标集
+import '@phosphor-icons/web/bold' // 使用 bold 图标集
 
 const app = createApp(App)
 app.mount('#app')
-
